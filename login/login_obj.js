c997cc088b2be8410c2c07338a932d676faef9bf
marcosvsantos08 <62360079+marcosvsantos08@users.noreply.github.com>
2020-08-26 20:54:05 -0300

    login 0.0.2

--- login/login_obj.js
+++ login/login_obj.js
@@ -1,16 +1,10 @@
 var objLogin = [ 
     {
-        usuario: "joao",
-        senha: "1234"
+        usuario: "marcos",
+        senha: "123456",
+        email:"marcosvsantos08@gmail.com"
     },
-    {
-        usuario: "caio",
-        senha: "senha"
-    },
-    {
-        usuario: "bruno",
-        senha: "senha123"
-    },
+    
 ]
  function getLogin(){
     var usuario = document.getElementById("usuario").value
@@ -18,10 +12,29 @@ var objLogin = [
  
  for (i=0; i < objLogin.length;i++){
      if (usuario==objLogin[i].usuario && senha==objLogin[i].senha){
-        console.log(usuario + " voce esta conectado")
+        alert(usuario + " voce esta conectado")
         return
     }
   }
   alert("usuario ou senha incorreto!")
 
  }
+ function cadastrar(){
+    let user=getElementById("user").value
+    let pass=getElementById("pass1").value
+    let pass1=getElementById("pass2").value
+    let mail=getElementById("email").value
+    
+    for (let n=0; n < objLogin.length;n++){
+        if (user==objLogin[n].usuario && mail==objLogin[n].email){
+           alert(`Usuario ou email ja existem`)
+        }else if(pass!==pass1){
+            alert(`Verifique a senha`)
+        }else{
+            objLogin.usuario.push(user)
+            objLogin.senha.push(pass)
+            objLogin.email.push(mail)
+            location.href = "file:///C:/Users/marco/OneDrive/Documentos/PROJETOS/login/login.html";
+        }
+ }
+}
